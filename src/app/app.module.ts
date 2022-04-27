b1f3f497aadf26e7a67b2ef0dd12b617837d7d09
Richman08 <oloktionov1985@gmail.com>
2022-04-27 19:16:06 +0200

    #00-control-icon (fix): added control icons, fixed conditional for validators, removed unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AxControlComponent } from "./shared/ui/input/ax-control.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { IconComponent } from "./shared/ui/icon/icon";
 import { ValidatorComponent } from "./shared/validators/components/validator/validator.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     AxControlComponent,
-    IconComponent,
     ValidatorComponent
   ],
   imports: [
--- src/app/shared/ui/icon/icon.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from "@angular/core";
-
-@Component({
-  selector: 'app-icon',
-  template: `<i class="material-icons">{{ icon }}</i>`,
-})
-export class IconComponent {
-  @Input() icon: string = ''
-}
